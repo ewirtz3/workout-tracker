bd047b5f50b1f241918e72b2ba074882466043f6
Emily Wirtz <ewirtz3@gmail.com>
2020-07-14 23:56:23 -0500

    adding model and routes

---
+++ routes/api.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then((newWorkout) => {
+      res.json(newWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workout", (req, res) => {
+  Workout.find({});
+});
+
+module.exports = router;
