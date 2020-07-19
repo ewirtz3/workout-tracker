cf84831569334a00f96066e0cec52e3e60e0058c
Emily Wirtz <ewirtz3@gmail.com>
2020-07-19 13:19:46 -0500

    pushing changes

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,79 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/exercise", ({ body }, res) => {
-  Workout.create(body)
-    .then((newWorkout) => {
-      res.json(newWorkout);
+//post workout route
+router.post("/workouts", ({ body }, res) => {
+  console.log("post route hit");
+  console.log("post route body:", body);
+  // Workout.create(body)
+  //   .then((newWorkout) => {
+  //     res.json(newWorkout);
+  //   })
+  //   .catch((err) => {
+  //     res.status(400).json(err);
+  //   });
+});
+
+//general get workouts route
+router.get("/workouts", (req, res) => {
+  console.log("get route hit general");
+  // console.log(`general get res:>>`, res.body);
+  Workout.find({})
+    .then((recentWorkout) => {
+      console.log(recentWorkout);
     })
     .catch((err) => {
-      res.status(400).json(err);
+      res.json(err);
     });
 });
 
-router.get("/api/workouts", (req, res) => {
+//update workout by id
+router.put("/workouts/:id", ({ body, params }, res) => {
+  console.log(`workout id route hit:>>`);
+  console.log("update body", body);
+  Workout.create(body).then(({ id }) =>
+    Workout.findOneAndUpdate(
+      id,
+      {
+        $push: {
+          exercises: body,
+        },
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    )
+      .then((updatedWorkout) => {
+        res.json(updatedWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      })
+  );
+});
+
+//get workouts in the last 7 days
+router.get("/workouts/range", (req, res) => {
+  console.log(`range get request hit`);
   Workout.find({})
-    .sort({ date: -1 })
-    .then((newWorkout) => {
-      res.json(newWorkout);
+    .limit(7)
+    .then((recentWorkouts) => {
+      res.json(recentWorkouts);
     })
     .catch((err) => {
-      res.status(400).json(err);
+      res.json(err);
+    });
+});
+
+//delete workout route
+router.delete("/workouts", ({ body }, res) => {
+  Workout.findOneAndDelete(body.id)
+    .then((deletedWorkout) => {
+      res.json(deletedWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
     });
 });
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const apiRoutes = require("./routes/api");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,13 +12,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
 
 //routes
-app.use(require("./routes/api"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
